3f33c676ffbf658fd4e4e2c4278625971ff2ccfa
ao19n <56572070+ao19n@users.noreply.github.com>
2021-10-13 09:37:49 +0900

    Update tst.js

--- tst.js
+++ tst.js
@@ -1,11 +1,11 @@
 const { json } = require('express');
 const fetch = require('node-fetch');
 const token = "{eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjJlM2M5NzY4LTEyODgtNDYzYy04ZGE5LTU1N2FkYTliMTA3ZCIsImlhdCI6MTYyMjA5ODI1MSwic3ViIjoiZGV2ZWxvcGVyL2NjYmQ3MjkyLWQ2YjAtOTUzOC00YTUyLThlZDU0NzhkODkxNyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTE4LjIzOC4yMzkuMjAyIl0sInR5cGUiOiJjbGllbnQifV19.POycW2dk4fVdzgKsrowSf28Q8PdgELESMHRaj6GqcwYDPJqEHfmIWki_OJopKOYHxbUUbGFmVvae_v9VUQdwCQ}";
-
+  //任意のトークン
 const URL = 'https://api.brawlstars.com/v1'
 
 const tag = '2LJL2V22Q';
-
+  //任意のユーザータグ
 const endpoint = URL + "/players/%23" + tag;
 
   fetch(endpoint, {
@@ -16,5 +16,6 @@ const endpoint = URL + "/players/%23" + tag;
       const obj = JSON.parse(JSON.stringify(json));
       console.log("name" + obj.name);
       console.log(obj.club.tag)
+    //任意の出力結果
     })
 
